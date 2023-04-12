f4b91f22a13101b03922880ddb1e81c984202639
Katelyn <kjcollins7@gmail.com>
2023-04-12 18:31:09 -0400

    bug fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const driverLogin = document.getElementById('driver-text');
 const loginForm = document.getElementById('login-form');
 
 function toggleLogin() {
-  loginForm.style.display = (loginForm.style.display === 'none') ? 'block' : 'none';
+  loginForm.style.display = (loginForm.style.display === 'block') ? 'none' : 'block';
 }
 
 driverLogin.addEventListener('click', toggleLogin);
